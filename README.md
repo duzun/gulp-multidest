c95339dd6598d99e9a12013c48912330bd0bb456
Dumitru Uzun <contact@duzun.me>
2016-03-17 20:31:13 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # gulp-multidest
-Equivalent of gulp.dest([path1, path2, ...])
+
+Equivalent of `gulp.dest(path)` with support of Array `path`.
+
+## Install
+
+```sh
+npm i gulp-multidest -D
+```
+
+## Usage
+
+```js
+
+var gulp = require('gulp');
+var gulp_dest = require('gulp-multi-dest');
+
+
+gulp.task('default', function() {
+    return gulp.src('./src/*.js')
+        // .pipe(process())
+        .pipe(gulp_dest(['./dist/', './build/', './test/'], { mode: 0777 }))
+    ;
+});
+
+```
---
+++ index.js
@@ -0,0 +1,42 @@
+/**
+ * Gulp multidestination piping.
+ *
+ * @license  MIT
+ * @author   Dumitru Uzun (DUzun.Me)
+ * @version  1.0.0
+ * @repo     https://github.com/duzun/gulp-multidest
+ */
+
+/* jshint node: true, -W041 */
+var PLUGIN_NAME = 'gulp-multidest';
+var lazypipe    = require('lazypipe');
+var gulp        = require('gulp');
+
+module.exports = function multidest(paths, options) {
+    var lazystream = lazypipe();
+
+    // Ensure paths is an array
+    if ( typeof paths == 'string' ) {
+        path = [paths];
+    }
+
+    // Clone `options` object, if provided
+    var opt = Object.assign({}, options ||{});
+
+
+    // Don't pipe twice to the same destination
+    var uniq = {};
+
+    paths.forEach(function (path, idx) {
+        if ( path == undefined ) return;
+
+        // normalize paths for comparison
+        var _path = String(path).replace(/[\/\\]+/g, '/').replace(/^\.\/|\/$/g, '');
+        if ( _path in uniq ) return;
+        uniq[_path] = path;
+
+        lazystream = lazystream.pipe(gulp.dest, path, opt);
+    });
+
+    return lazystream();
+};

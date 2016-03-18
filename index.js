f52681a2819049bc51ccfccba3ba12c2c33e81c8
Dumitru Uzun <contact@duzun.me>
2016-03-18 13:16:44 +0200

    Throw error if paths is empty

--- index.js
+++ index.js
@@ -3,18 +3,20 @@
  *
  * @license  MIT
  * @author   Dumitru Uzun (DUzun.Me)
- * @version  1.0.1
+ * @version  1.0.2
  * @repo     https://github.com/duzun/gulp-multidest
  */
 
 /* jshint node: true, -W041 */
-var PLUGIN_NAME = 'gulp-multidest';
 var lazypipe    = require('lazypipe');
 var gulp        = require('gulp');
 
-module.exports = function multidest(paths, options) {
-    var lazystream = lazypipe();
+const PLUGIN_NAME = 'gulp-multidest';
 
+var forEach = [].forEach;
+var filter  = [].filter;
+
+module.exports = function multidest(paths, options) {
     // Ensure paths is an array
     if ( typeof paths == 'string' ) {
         paths = [paths];
@@ -23,18 +25,30 @@ module.exports = function multidest(paths, options) {
     // Clone `options` object, if provided
     var opt = Object.assign({}, options ||{});
 
-
     // Don't pipe twice to the same destination
     var uniq = {};
-
-    paths.forEach(function (path, idx) {
-        if ( path == undefined ) return;
+    paths = filter.call(paths, function (path) {
+        if ( path == undefined ) return false;
 
         // normalize paths for comparison
         var _path = String(path).replace(/[\/\\]+/g, '/').replace(/^\.\/|\/$/g, '');
-        if ( _path in uniq ) return;
+        if ( _path in uniq ) return false;
         uniq[_path] = path;
+        return true;
+    });
 
+    // No paths, no dest
+    if ( paths.length == 0 ) {
+        throw new Error(PLUGIN_NAME + ': paths arguments is empty');
+    }
+
+    // If there is only one destination, just return gulp.dest(path, options)
+    if ( paths.length == 1 ) {
+        return gulp.dest(paths[0], opt);
+    }
+
+    var lazystream = lazypipe();
+    forEach.call(paths, function (path) {
         lazystream = lazystream.pipe(gulp.dest, path, opt);
     });
 
